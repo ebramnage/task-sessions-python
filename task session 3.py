aa22bbff0aa2d334d72dc5c180d439098212b9bd
ebramnage <nageebram52@gmail.com>
2025-09-02 16:55:49 +0300

    Add files via upload

---
+++ task session 3.py
@@ -0,0 +1,17 @@
+x=25
+if 30>x>20:
+    print("its a warm day.enjou the weather!")
+x=30
+if 30<=x:
+    print("its a hot day. stay hydrated!")
+x=15
+if 20>x>10:
+    print("its cool day.wear a jacket!")
+x=8
+if x<10:
+    print("its a cold day.stay warm!" )
+print("---------------------------------")
+for i in range(1,21):
+    if i%3==0:
+        print(i)
+print("numbers divisable by 3")
